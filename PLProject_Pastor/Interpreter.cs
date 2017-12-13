4237a79654a01662d933da30c5e1d27c3e2204d1
UnbreakableSigns <johnpast2r@gmail.com>
2017-12-13 23:20:53 +0800

    separated functions

--- PLProject_Pastor/Interpreter.cs
+++ PLProject_Pastor/Interpreter.cs
@@ -23,99 +23,69 @@ namespace PLProject_Pastor
         Dictionary<string, bool> boolType = new Dictionary<string, bool>();
 
         string output;
+        bool consoleIsOpen = false;
+
         public void read(string code)
         {
+            consoleIsOpen = false;
+
             output = "";
             stringType = new Dictionary<string, string>();
             numberType = new Dictionary<string, double>();
             string[] splitArray = Regex.Split(code, @"(?:,\s+)");
             List<string> statements = new List<string>();
 
-            bool consoleIsOpen = false;
 
             for (int i = 0; i < splitArray.Length; i++)
             {
-                statements.AddRange(splitArray[i].Split('\n', '\t', '='));
+                statements.AddRange(splitArray[i].Split('\n', '\t'));
             }
 
             for (int i = 0; i < statements.Count; i++)
             {
-                //token: token
+                //token[0]: token[1]
                 List<string> tokens = new List<string>();
                 tokens.AddRange(statements.ElementAt(i).Split(':'));
                 tokens[0] = tokens[0].Trim();
                 tokens[1] = tokens[1].Trim();
 
-                if (tokens[0].Trim().StartsWith(datatypes[0]))//number
+                //----------------------------------------------------NUMBER
+                if (tokens[0].Trim().StartsWith(datatypes[0]))
                 {
                     numberType.Add(tokens[0], int.Parse(tokens[1]));
                 }
-                else if (tokens[0].Trim().StartsWith(datatypes[1]))//list
+                //----------------------------------------------------LIST
+                else if (tokens[0].Trim().StartsWith(datatypes[1]))
                 {
                     List<double> listOfDouble = new List<double>();
                     //TODO:
                     //split tokens[1] into list of numbers
                     listType.Add(tokens[0], listOfDouble);
                 }
-                else if (tokens[0].Trim().StartsWith(datatypes[2]))//string
+                //----------------------------------------------------STRING
+                else if (tokens[0].Trim().StartsWith(datatypes[2]))
                 {
                     stringType.Add(tokens[0], tokens[1]);
                     MessageBox.Show("[" +tokens[0] + "] added to string [" + tokens[1] + "]");
                 }
-                else if (tokens[0].Trim().StartsWith(datatypes[3]))//bool
+                //----------------------------------------------------BOOL
+                else if (tokens[0].Trim().StartsWith(datatypes[3]))
                 {
                     boolType.Add(tokens[0], bool.Parse(tokens[1]));
                 }
-                if (tokens[0].Contains(keywords[0]))//read
+                //----------------------------------------------------READ
+                else if (tokens[0].Contains(keywords[0]))
                 {
-                    if (consoleIsOpen == false)
-                    {
-                        ConsoleOutput.OpenConsole();
-                        consoleIsOpen = true;
-                    }
-                    string value = ConsoleOutput.ReadLineConsole();
-                    if (stringType.ContainsKey(tokens[0]))
-                    {
-                        stringType.Remove(tokens[0]);
-                        stringType.Add(tokens[0], value);
-                    }
+                    Read(tokens);
                 }
-                else if (tokens[0].Contains(keywords[1]))//write
+                //----------------------------------------------------WRITE
+                else if (tokens[0].Contains(keywords[1]))
                 {
-                    if (consoleIsOpen == false)
-                    {
-                        ConsoleOutput.OpenConsole();
-                        consoleIsOpen = true;
-                    }
-                    if (tokens[1].StartsWith("\'"))
-                    {
-                        output = tokens[1].Substring(1, tokens[1].Length - 2) + "\n";
-                        ConsoleOutput.WriteConsole(output);
-                    }
-                    else if (stringType.ContainsKey(tokens[0].Trim()))
-                    {
-                        MessageBox.Show("pasok");
-                        if (stringType[tokens[1]].Trim().ToString().StartsWith("\'"))
-                        {
-                            output = stringType[tokens[1]].Trim().ToString().Substring(1, stringType[tokens[1]].Trim().ToString().Length - 2) + "\n";
-                            ConsoleOutput.WriteConsole(output);
-                        }
-                        else
-                        {
-                            output = stringType[tokens.ElementAt(1)] + "\n";
-                            ConsoleOutput.WriteConsole(output);
-                        }
-                    }
-/*                    else if (numberType.ContainsKey(tokens.ElementAt(i + 1)))
-                    {
-                        output = numberType[tokens.ElementAt(i + 1)] + "\n";
-                        ConsoleOutput.WriteConsole(output);
-                    }
-                    */
+                    Write(tokens);
                 }
 
 
-
+                //Display Console
                 if (consoleIsOpen == true)
                 {
                     ConsoleOutput.WriteConsole("\n\n--------------------------------------------------------------------------------\nPress any key to continue. . .");
@@ -126,5 +96,53 @@ namespace PLProject_Pastor
             }
 
         }
+
+        void Read(List<string> tokens) {
+            if (consoleIsOpen == false)
+            {
+                ConsoleOutput.OpenConsole();
+                consoleIsOpen = true;
+            }
+            string value = ConsoleOutput.ReadLineConsole();
+            if (stringType.ContainsKey(tokens[0]))
+            {
+                stringType.Remove(tokens[0]);
+                stringType.Add(tokens[0], value);
+            }
+        }
+
+        void Write(List<string> tokens) {
+            if (consoleIsOpen == false)
+            {
+                ConsoleOutput.OpenConsole();
+                consoleIsOpen = true;
+            }
+            if (tokens[1].StartsWith("\'"))
+            {
+                output = tokens[1].Substring(1, tokens[1].Length - 2) + "\n";
+                ConsoleOutput.WriteConsole(output);
+            }
+            else if (stringType.ContainsKey(tokens[0].Trim()))
+            {
+                MessageBox.Show("pasok");
+                if (stringType[tokens[1]].Trim().ToString().StartsWith("\'"))
+                {
+                    output = stringType[tokens[1]].Trim().ToString().Substring(1, stringType[tokens[1]].Trim().ToString().Length - 2) + "\n";
+                    ConsoleOutput.WriteConsole(output);
+                }
+                else
+                {
+                    output = stringType[tokens.ElementAt(1)] + "\n";
+                    ConsoleOutput.WriteConsole(output);
+                }
+            }
+            /*                    else if (numberType.ContainsKey(tokens.ElementAt(i + 1)))
+                                {
+                                    output = numberType[tokens.ElementAt(i + 1)] + "\n";
+                                    ConsoleOutput.WriteConsole(output);
+                                }
+                                */
+        }
+
     }
 }
